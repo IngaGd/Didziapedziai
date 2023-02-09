9d1cd0a3a86dd3f4ec832c3f27e471decf656ed8
Inga <inga.gudaite@gmail.com>
2023-02-09 12:03:33 +0200

    React

---
+++ r1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Cat from './Components/006/Cat';
+import Racoon from './Components/006/Racoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Racoon></Racoon>
+        <Racoon/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
