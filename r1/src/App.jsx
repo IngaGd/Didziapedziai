3bcda113a578c469782637f2f5ba1a1f73674f5b
Inga <inga.gudaite@gmail.com>
2023-02-20 12:02:39 +0200

    Homeworks

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,25 +1,11 @@
 import './App.scss';
-import LabasZuiki from './Homeworks/01-React-base/LabasZuiki';
-import SimpleProps from './Homeworks/01-React-base/SimpleProps';
-import Task4 from './Homeworks/01-React-base/Task4';
-import Task5 from './Homeworks/01-React-base/Task5';
-import ZebraiBebrai from './Homeworks/01-React-base/ZebraiBebrai';
-
 
 function App() {
-
 
     return (
         <div className="App">
             <header className="App-header">
-                <LabasZuiki/>
-                <SimpleProps text={'This is a text for simple props'}/>
-                <ZebraiBebrai setColor='2'/>
-                <Task4 props1 = 'cia bet kloks tekstas h1 tagui' props2 = 'cia bet kloks tekstas h2 tagui'/>
-                <Task5 props1 = 'cia bet kloks tekstas h1 tagui' 
-                props2 = 'cia bet kloks tekstas h2 tagui'
-                color = 'green'
-                />
+
             </header>
         </div>
     );
