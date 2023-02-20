56d6c1609ae66f071754a994f19c9a19d328987b
Inga <inga.gudaite@gmail.com>
2023-02-20 13:30:35 +0200

    Composition, circles with numbers

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,23 @@
+'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import Circle from './Components/011/Circle';
+import Number from './Components/011/Number';
 
 function App() {
+
 
     return (
         <div className="App">
             <header className="App-header">
+
+            <Circle>
+                <Number type="n1"/>
+                <Number type="n2"/>
+                <Number type="n3"/>
+                <Number type="n4"/>
+            </Circle>
+            
+                
 
             </header>
         </div>
@@ -12,4 +25,4 @@ function App() {
 
 }
 
-export default App; 
+export default App;
