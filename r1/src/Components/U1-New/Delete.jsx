e646e00c771d823cb4e6cc44d8e6aaad4bb0ccb0
Inga <inga.gudaite@gmail.com>
2023-03-08 10:02:55 +0200

    U1 refactor

--- r1/src/Components/U1-New/Delete.jsx
+++ r1/src/Components/U1-New/Delete.jsx
@@ -2,9 +2,8 @@ function Delete({setDeleteModal, setDeleteData, account}) {
 
     return (
         <div className="delete-modal">
-            Confirm delete
-            <span onClick={() => setDeleteData(account)}>X</span>
-            <span onClick={() => setDeleteModal(null)}>Cancel</span>            
+            <span className="delete" onClick={() => setDeleteData(account)}>Confirm delete</span>
+            <span className="delete" onClick={() => setDeleteModal(null)}>Cancel</span>            
         </div>
     )
 
