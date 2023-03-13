8cf69c2cc514adfc1a2efa4dc9429ab7e54bea79
Inga <inga.gudaite@gmail.com>
2023-03-13 18:41:43 +0200

    Custom hooks, comments

--- r1/src/Components/019/useAdd23.jsx
+++ r1/src/Components/019/useAdd23.jsx
@@ -5,12 +5,13 @@ import { useState } from "react"
 export const useAdd23 = init => { // init - pradine reiksme nuo kurios prades pridedinet
 
     const [value, setValue] = useState(init);
-    const [status, setStatus] = useState(true);
+    const [status, setStatus] = useState(true); //set status kontroliuoja c => !c, t.y. c bus tai true tai false,
+    //status bus tai true tai false.
 
 
     useEffect(() => {
         setValue(v => v + 23); //cia uzfiksuojame matematika, kuri yra atliekama
-        //skirta prie pradinio state prideti 23
+        //skirta prie pradinio state prideti 23. status pokytis issauks sios funkcijos veikima
     }, [status])
 
     //seteris ne tos reiksmes
--- r1/src/Components/019/useLocalStorage.jsx
+++ r1/src/Components/019/useLocalStorage.jsx
@@ -1,18 +1,21 @@
 import { useEffect } from "react";
-import { useState } from "react"
+import { useState } from "react";
 
 
 
-export const useLocalStorage = (lsKey, init) => {
+export const useLocalStorage = (lsKey, init) => { //pries irasant reiksme, pradine useState reiksme yra tokia, kokia
+    //jis gauna is local storage, todel pries irasant reiksme, reikia nueiti i local storage ir ja pasiimti.
+    //Nirint nueiti i localstorage, dar reikia localstorage keyjaus - lsKey.
 
     const [value, setValue] = useState(() => {
         const data = localStorage.getItem(lsKey);
-        return null === data ? init : JSON.parse(data);
+        return null === data ? init : JSON.parse(data); //jeu null === data (t.y. localstorage tuscias),
+        //grazinam pradine reiksme, kurios norime (init), jei ne tuscias, grazinam suparsindami data
     });
 
     useEffect(() => {
-        localStorage.setItem(lsKey, JSON.stringify(value)); 
-    }, [value, lsKey]);
+        localStorage.setItem(lsKey, JSON.stringify(value)); //irasome i localstorage pasikeitusia reiksme
+    }, [value, lsKey]); //dedam lsKey i dependensius, nes jis nesikaitalioja, yra stringas is esmes
 
     return [value, setValue];
 
